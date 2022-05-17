5924e7773cef4ecf914e9a4fbbac69f09af6514b
kitoyjami <weurbanor@unitru.edu.pe>
2022-05-17 14:27:43 -0500

    se agrego nuevo proyecto

--- ejemplo01/src/Titulo.jsx
+++ ejemplo01/src/Titulo.jsx
@@ -8,15 +8,3 @@ const Titulo = () => {
     </View>
   )
 }
-
-export default Titulo
-
-import React, { Component } from 'react'
-
-export default class Titulo extends Component {
-  render() {
-    return (
-      <div>Titulo</div>
-    )
-  }
-}
